82819f58e6769d97f996b55384e1d06ee8165373
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2025-05-04 17:04:26 +0300

    Hallinnan etusivu

---
+++ web/src/app/hallinta/page.tsx
@@ -0,0 +1,23 @@
+import { getLoggedInUser } from '@/services/userSession';
+
+const HallintaPage = async () => {
+  const user = await getLoggedInUser();
+
+  if (!user) {
+    return (
+      <div>
+        <h1>Etusivu</h1>
+        <p>Et ole kirjautunut sisään.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Hallinta</h1>
+      <p>Tervetuloa {user.name} hallintapaneeliin!</p>
+    </div>
+  );
+};
+
+export default HallintaPage;
