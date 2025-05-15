c391ec7d98ff29d48656ccd9ee9167d127a7dfdc
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2025-05-15 18:33:58 +0300

    Oikeat ehdot tilauskoodille

--- web/src/schemas/orderCode.schema.ts
+++ web/src/schemas/orderCode.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const codeSchema = z
   .string()
   .min(3, { message: 'Koodi on vähintään 3 merkkiä' })
-  .max(16, { message: 'Koodi on enintään 16 merkkiä' })
+  .max(36, { message: 'Koodi on enintään 36 merkkiä' })
   .regex(/^[a-zA-Z0-9]+$/, {
     message: 'Koodissa saa olla vain aakkosia ja numeroita',
   });
@@ -13,10 +13,7 @@ export const createCodeSchema = z.object({
     .string()
     .min(1, { message: 'Nimi vaaditaan.' })
     .max(128, { message: 'Nimi voi olla enintään 128 merkkiä.' }),
-  code: z
-    .string()
-    .min(1, { message: 'Koodi vaaditaan.' })
-    .max(128, { message: 'Koodi voi olla enintään 128 merkkiä.' }),
+  code: codeSchema,
   availableOrders: z.coerce
     .number({ invalid_type_error: 'Kentän tulee olla numero.' })
     .int({ message: 'Kentässä tulee olla kokonaisluku.' })
