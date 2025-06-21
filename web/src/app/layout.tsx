87f832bb70dfb8af0b44b3c338acebe65ca49160
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2025-06-21 21:46:58 +0300

    Seuranta

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -9,6 +9,7 @@ import { Settings, SettingsDatabase } from '@/interfaces/settings.interface';
 import ToastMessage from '@/components/ToastMessage';
 import SignOutButton from '@/components/SignOutButton';
 import { getLoggedInUser } from '@/services/userSession';
+import PlausibleProvider from 'next-plausible';
 
 const montserrat = Montserrat({
   variable: '--font-montserrat',
@@ -35,25 +36,27 @@ export default async function RootLayout({
   const menuItems = settings.data?.[0].mainMenu.menuItems ?? [];
 
   return (
-    <html lang="fi">
-      <head>
-        <meta name="theme-color" content="#ffffff" />
-      </head>
-      <body
-        className={`${montserrat.variable} ${montserrat.variable} antialiased bg-gradient-to-b from-foreground to-background bg-fixed min-h-dvh flex flex-col gap-4 md:gap-6`}
-      >
-        <header className="w-full min-h-16 md:h-fit px-4 sm:px-8 md:px-0 md:container md:mx-auto md:max-w-3xl">
-          <ToastMessage />
-          <NavigationMenuButton>
-            <NavigationMenu items={menuItems}>
-              <SignOutButton user={user} />
-            </NavigationMenu>
-          </NavigationMenuButton>
-        </header>
-        <main className="container max-w-4xl mx-auto px-4 sm:px-8 md:px-0 grow flex flex-col pb-8">
-          {children}
-        </main>
-      </body>
-    </html>
+    <PlausibleProvider domain="tarratoimikunta.fi" enabled={true}>
+      <html lang="fi">
+        <head>
+          <meta name="theme-color" content="#ffffff" />
+        </head>
+        <body
+          className={`${montserrat.variable} ${montserrat.variable} antialiased bg-gradient-to-b from-foreground to-background bg-fixed min-h-dvh flex flex-col gap-4 md:gap-6`}
+        >
+          <header className="w-full min-h-16 md:h-fit px-4 sm:px-8 md:px-0 md:container md:mx-auto md:max-w-3xl">
+            <ToastMessage />
+            <NavigationMenuButton>
+              <NavigationMenu items={menuItems}>
+                <SignOutButton user={user} />
+              </NavigationMenu>
+            </NavigationMenuButton>
+          </header>
+          <main className="container max-w-4xl mx-auto px-4 sm:px-8 md:px-0 grow flex flex-col pb-8">
+            {children}
+          </main>
+        </body>
+      </html>
+    </PlausibleProvider>
   );
 }
