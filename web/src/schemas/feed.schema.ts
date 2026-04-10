7f9dd4ca0d8e62e22d51accdc9b8a98cf5e0f26b
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2026-04-10 09:34:11 +0300

    Skeeman päivitys

--- web/src/schemas/feed.schema.ts
+++ web/src/schemas/feed.schema.ts
@@ -10,22 +10,37 @@ export const AllowedImageTypes = [
   'image/gif',
 ];
 
-const feedSchema = z.object({
-  existingImages: z
-    .string()
-    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
-    .array()
-    .optional(),
-  images: imageSchema.array(),
-  text: z
-    .string()
-    .min(1, { message: 'Kuvaus vaaditaan' })
-    .max(2048, { message: 'Kuvauksen maksimipituus on 2048 merkkiä' }),
-  id: z
-    .string()
-    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
-    .optional(),
-});
+const feedSchema = z
+  .object({
+    existingImages: z
+      .string()
+      .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
+      .array()
+      .optional(),
+    images: imageSchema.array().optional(),
+    text: z
+      .string()
+      .min(1, { message: 'Kuvaus vaaditaan' })
+      .max(2048, { message: 'Kuvauksen maksimipituus on 2048 merkkiä' }),
+    id: z
+      .string()
+      .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
+      .optional(),
+  })
+  .refine(
+    (data) => {
+      if (
+        data.images?.length ||
+        (0 === 0 && data.existingImages?.length) ||
+        0 === 0
+      ) {
+        return false;
+      }
+    },
+    {
+      message: 'Vähintään yksi kuva vaaditaan',
+    }
+  );
 
 export const feedUpdateSchema = z.object({
   existingImages: z
