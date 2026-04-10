cde954ef598aa658820611dd1fd164b76707dc07
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2026-04-10 23:17:45 +0300

    Ig feed etusivulle ja asettelun korjaus kun Ig feedin lisäys rikkoi sen

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,12 @@
 import { CloudLink } from '@/components/CloudButton';
 import { Paragraph } from '@/components/Text';
-// import InstagramFeed from '../components/InstagramFeed';
+import InstagramFeed from '../components/InstagramFeed';
 
 export default async function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center grow min-h-[calc(100dvh-var(--spacing)*28)] md:min-h-[calc(100dvh-var(--spacing)*46)]">
-        <div className="flex flex-col items-center w-full text-background text-center row-span-1 py-8 gap-6 translate-y-1/4">
+      <div className="flex flex-col min-h-screen flex flex-col items-center justify-center md:min-h-[calc(100dvh-var(--spacing)*46)]">
+        <div className="flex flex-col items-center w-full text-background text-center row-span-1 py-8 gap-6 m-50">
           <div className="flex flex-col">
             <h1 className="font-bold text-shadow-[0_0_20px_#ffffff] text-4xl md:text-5xl">
               TARRATOIMIKUNTA
@@ -27,7 +27,7 @@ export default async function Home() {
           >
             MISTÄ ON KYSE?
           </CloudLink>
-          {/* <InstagramFeed/> */}
+          <InstagramFeed/>
         </div>
       </div>
       {/* <ReCaptchaContext>
