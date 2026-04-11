ccbc5728a7b0c73a35c589af1598cab733877daf
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2026-04-11 13:49:24 +0300

    Siluetti järkevämmäksi

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,9 +6,9 @@ import Siluet from '@/components/Siluet';
 export default async function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center grow min-h-[calc(100vh-var(--spacing)*28)] md:min-h-[calc(100vh-var(--spacing)*46)] md:min-h-[calc(100vh-var(--spacing)*46)] md:mb-40 lg:mb-[calc(var(--spacing)*104)] xl:mb-[calc(var(--spacing)*120)]">
-        <div className="flex flex-col items-center w-full text-foreground text-center row-span-1 py-8 gap-6 translate-y-1/12">
-          <div className="flex flex-col">
+      <div className="flex flex-col justify-center grow min-h-[calc(100vh-var(--spacing)*20)] md:min-h-[calc(100vh-var(--spacing)*32)]">
+        <div className="flex flex-col gap-6 items-center py-8 row-span-1 text-foreground text-primary-foreground text-shadow-[0_0_20px_#ffffff] translate-y-[4.3%] w-full">
+          <div className="flex flex-col text-center w-full">
             <h1 className="font-bold text-shadow-[0_0_20px_#ffffff] text-4xl md:text-5xl">
               TARRATOIMIKUNTA
             </h1>
@@ -16,7 +16,7 @@ export default async function Home() {
               SUVISEUROISSA 2026
             </p>
           </div>
-          <Paragraph className="text-center text-shadow-[0_0_20px_#ffffff] text-xl font-light">
+          <Paragraph className="font-light text-center text-shadow-[0_0_20px_#ffffff] text-xl w-full">
             Tervetuloa turvasatamaan. Me olemme Tarratoimikunta. Seisomme
             ihmisoikeuksien ja heikompien puolella Jeesuksen tavoin.{' '}
           </Paragraph>
@@ -32,6 +32,7 @@ export default async function Home() {
         <Siluet />
       </div>
       <div className="bg-primary pt-8 relative">
+        <div className="absolute h-36 top-0 left-0 w-full gradient-from-background-to-transparent"></div>
         <div className="container max-w-5xl mx-auto px-4 sm:px-8 md:px-8 lg:px-0">
           <InstagramFeed />
         </div>
--- web/src/components/Siluet/index.tsx
+++ web/src/components/Siluet/index.tsx
@@ -2,19 +2,13 @@ import Image from 'next/image';
 
 const Siluet = () => {
   return (
-    <div
-      className="absolute h-screen top-0 w-full -z-10 left-0"
-      style={{
-        backgroundImage:
-          'linear-gradient(180deg,rgba(144, 174, 200, 1) 0%, rgba(242, 216, 189, 1) 31%, rgba(250, 201, 98, 1) 61%, rgba(246, 208, 140, 1) 79%, rgba(252, 158, 84, 1) 99%)',
-      }}
-    >
+    <div className="absolute h-screen top-0 w-full -z-10 left-0 gradient">
       <Image
         src="/siluetti_demo.png"
         alt="Siluet"
         width={1074}
         height={452}
-        className="absolute w-full object-cover left-0 height-auto bottom-0 lg:bottom-[-20%] xl:bottom-[-30%] 2xl:bottom-[-40%]"
+        className="absolute w-full object-cover left-0 height-auto bottom-0 xl:hidden"
         unoptimized={true}
       />
     </div>
