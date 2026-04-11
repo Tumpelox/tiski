045dfa38cb98d61e40686ff1cc96c76cf4634275
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2026-04-11 23:33:12 +0300

    Linkki pois

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     <>
       <div className="flex flex-col justify-center grow min-h-[calc(100svh-var(--spacing)*16)] md:min-h-[calc(100svh-var(--spacing)*28)]">
         <div className="flex flex-col gap-6 items-center py-8 row-span-1 text-foreground text-shadow-[0_0_20px_var(--foreground)] w-full">
-          <div className="flex flex-col text-center w-full">
+          <div className="flex flex-col text-center w-full px-4">
             <h1 className="font-bold text-4xl md:text-5xl">TARRATOIMIKUNTA</h1>
             <p className="text-3xl md:text-4xl font-light">
               SUVISEUROISSA 2026
@@ -18,14 +18,14 @@ export default async function Home() {
             Tervetuloa turvasatamaan. Me olemme Tarratoimikunta. Seisomme
             ihmisoikeuksien ja heikompien puolella Jeesuksen tavoin.
           </Paragraph>
-          <CloudLink
+          {/* <CloudLink
             className="mt-4 md:mt-8"
             variant={'card'}
             size={'large'}
             href="/sivut/mista-on-kyse"
           >
             MISTÄ ON KYSE?
-          </CloudLink>
+          </CloudLink> */}
         </div>
         <Siluet />
       </div>
