dd26681d570752816892c50cb40544d9c456f864
Tuomas Tornberg <68976612+Tumpelox@users.noreply.github.com>
2026-04-13 18:50:10 +0300

    Tekstikoon vaihtelu ongelma pois

--- web/src/components/InstagramPost.tsx
+++ web/src/components/InstagramPost.tsx
@@ -18,7 +18,7 @@ const InstagramPost = ({ post }: { post: FeedDocument }) => {
         <div className="grow px-4 bg-card text-card-foreground rounded-md pt-4 pb-2">
           <MarkdownToHtml
             markdown={
-              open ? post.text : String(post.text).slice(0, 250) + '...'
+              open ? post.text : String(post.text).slice(0, 250) + `...\n`
             }
           />
           {String(post.text).length > 250 && (
